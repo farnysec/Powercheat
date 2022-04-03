94820653746e7fae177960a18ef2b81af354d0e3
far_n_y <33955667+farnysec@users.noreply.github.com>
2022-04-03 12:06:12 +0100

    Add files via upload

--- Basic_sec_PS_Cheatsheet.ps1
+++ Basic_sec_PS_Cheatsheet.ps1
@@ -11,6 +11,64 @@
 Get-variable -Verbose
 
 
+#FILES MODIFIED IN THE LAST 4 DAYS
+================================================================================
+Get-ChildItem c: -Recurse | ? {$_.LastWriteTime -gt (Get-Date).AddDays(-4)} | Sort-Object -Property LastWriteTime | Format-List LastWriteTime, FullName
+
+
+#PROCESS DUMP FROM MEMORY - EXAMPLE WITH LSASS
+================================================================================
+$Mylsass = Get-Process -Name lsass
+C:\Windows\System32\rundll32.exe C:\Windows\System32\comsvcs.dll, MiniDump $Mylsass.id c:\somewhere\dumplsass.bin full
+
+
+#LIST ACCOUNTS ON THE SYSTEM
+================================================================================
+Get-LocalUser
+
+
+#LIST ACCOUNTS ON THE SYSTEM BY LAST LOGON
+================================================================================
+Get-WmiObject -Class Win32_NetworkLoginProfile | 
+Sort-Object -Property LastLogon -Descending | Select-Object -Property * 
+
+
+SIGNED SCRIPTS
+================================================================================
+#Check execution policy
+Get-ExecutionPolicy
+#Set the policy to execute scripts
+Set-ExecutionPolicy [policy]
+
+
+FIND OUT WHO DID WHAT IN ACTIVE DIRECTORY - AUDITING MUST BE ENABLED.
+================================================================================
+#Simple GPO deployed over DCs OU -> Computer Configuration -> Policies -> Windows Settings -> Security Settings -> Advanced Audit Configuration
+
+Find-Events -Report ADGroupMembershipChanges -DatesRange Last3days -Servers AD1, AD2 | Format-Table -AutoSize
+
+#ReportTypes:
+#Computer changes – Created / Changed – ADComputerCreatedChanged
+#Computer changes – Detailed – ADComputerChangesDetailed
+#Computer deleted – ADComputerDeleted
+#Group changes – ADGroupChanges
+#Group changes – Detailed – ADGroupChangesDetailed
+#Group changes – Created / Deleted – ADGroupCreateDelete
+#Group enumeration – ADGroupEnumeration
+#Group membership changes – ADGroupMembershipChanges
+#Group policy changes – ADGroupPolicyChanges
+#Logs Cleared Other – ADLogsClearedOther
+#Logs Cleared Security – ADLogsClearedSecurity
+#User changes – ADUserChanges
+#User changes detailed – ADUserChangesDetailed
+#User lockouts – ADUserLockouts
+#User logon – ADUserLogon
+#User logon Kerberos – ADUserLogonKerberos
+#User status changes – ADUserStatus
+#User unlocks – ADUserUnlocked
+
+
+
 #WIN EVENTS TO MONITOR
 ================================================================================
 $sec_events = @{} 
@@ -25,19 +83,4 @@ $sec_events.Add("4728",	"A member was added to a security-enabled global group")
 $sec_events.Add("4732",	"A member was added to a security-enabled local group")
 
 Get-EventLog Security | Where-Object -FilterScript {$_.EventID -in $sec_events.Keys}
-
-
-#FILES MODIFIED IN THE LAST 4 DAYS
-================================================================================
-Get-ChildItem c: -Recurse | ? {$_.LastWriteTime -gt (Get-Date).AddDays(-4)} | Sort-Object -Property LastWriteTime | Format-List LastWriteTime, FullName
-
-
-
-#LIST ACCOUNTS ON THE SYSTEM
-================================================================================
-Get-LocalUser
-
-#
-================================================================================
-Get-LocalUser
 
