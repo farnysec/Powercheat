00663af352e13ba8791edfd65a5c7d881f8c68f2
far_n_y <33955667+farnysec@users.noreply.github.com>
2022-05-29 02:11:34 +0100

    Added bubble sort
    Added bubble sort

--- Algo_PS_Cheatsheet.ps1
+++ Algo_PS_Cheatsheet.ps1
@@ -9,6 +9,7 @@
 
 #FIBONACCI NUMBER
 ================================================================================
+
 $previous2_value = 0 ; $previous1_value = 1 ; $previous2_value ; $previous1_value
 for ($i =0 ; $i -lt 10 ; $i++) { $current_value = $previous1_value + $previous2_value ; $previous2_value = $previous1_value ; $previous1_value = $current_value ; $current_value }
 
@@ -30,8 +31,21 @@ $current_value
 #BUBBLE SORT
 ================================================================================
 
+function Bubble_Sort($targetList) {
+        $n = $targetList.Count
 
-
+        for ($i = 0; $i -lt $n; $i+=1) {
+            for ($j = 0; $j -lt $n-1; $j+=1) {
+                if($targetList[$j] -gt $targetList[$j+1]) {
+                    $temp = $targetList[$j+1]
+                    $targetList[$j+1]=$targetList[$j]
+                    $targetList[$j]=$temp
+                }
+            }
+        }
+		write-host ($targetList)
+		return ($targetList)
+    }
 
 
 #QUICK SORT
